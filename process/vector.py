426c551d83b1549085a9017d3f433c1159310249
pmiller10 <pmiller987@gmail.com>
2014-02-12 00:44:30 +0000

    VectorSpace class

--- process/vector.py
+++ process/vector.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy
 from random import shuffle
 
 def random_vector(vector_length):
@@ -10,10 +10,10 @@ def random_vector(vector_length):
         raise Exception('vector_length must be a multiple of 100')
     nonzero_count = vector_length * 0.02
     zero_count = vector_length - nonzero_count
-    z = zeros(zero_count)
-    positives = ones(nonzero_count/2)
-    negs = ones(nonzero_count/2)
-    merged = array(list(z) + list(positives) + list(negs))
+    z = numpy.zeros(zero_count)
+    positives = numpy.ones(nonzero_count/2)
+    negs = numpy.ones(nonzero_count/2) * -1
+    merged = numpy.array(list(z) + list(positives) + list(negs))
     shuffle(merged)
     return merged
 
@@ -26,25 +26,41 @@ def empty_vectors(words, vector_length):
     words = set(words) # unique them
     dict = {}
     for w in words:
-        dict[w] = zeros(vector_length)
+        dict[w] = numpy.zeros(vector_length)
     return dict
 
 def word_vectors(corpus, vector_length):
     """
     Takes a list of words and an integer as input.
     """
-    count = 0
-    empty = empty_vectors(corpus, vector_length)
+    words = {}
     for word in corpus:
-        if count % 10 == 0:
-            context_vector = random_vector(vector_length)
-        empty[word] += context_vector
-        count += 1
-    return empty
+        words[word] = random_vector(vector_length)
+    return words
 
 def cosine(v1,v2):
-    return float(dot(v1,v2) / (linalg.norm(v1) * linalg.norm(v2)))
+    return float(numpy.dot(v1,v2) / (numpy.linalg.norm(v1) * numpy.linalg.norm(v2)))
+
+class VectorSpace:
+
+    def __init__(self, unique_words, vector_length=100):
+        self.vectors = word_vectors(unique_words, vector_length)
+        self.vector_length = vector_length 
+
+    def vector(self, document):
+        """
+        Takes a list of words as input.
+        Returns the vector sum of each of those words.
+        """
+        document_vector = numpy.zeros(self.vector_length)
+        for word in document:
+            if word in self.vectors:
+                document_vector += self.vectors[word]
+            else:
+                print Warning("'{0}' not in self.vectors".format(word))
+        return document_vector
 
 if __name__ == "__main__":
     words = ['hey', 'hi', 'bye']
-    print word_vectors(words, 100)
+    vs = VectorSpace(words)
+    print vs.vector(['hey', 'hey', 'bye', 'x'])
